0ba412ee15e4818eecea9495218965e8218c0903
pablofon <153851706+pablofon@users.noreply.github.com>
2024-02-09 15:44:52 +0100

    AudioManager script base

---
+++ CavemanURP/Assets/ProjectoManhattan_Root/Scripts/AudioManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    //Declaración de Singletone
+    public static AudioManager Instance;
+
+    [Header("uidio Source References")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sfxSource;
+
+    [Header("Audio Clip Arrays")]
+    public AudioClip[] musicList;
+    public AudioClip[] sfxList;
+
+    private void Awake()
+    {
+        //Singletone que no se destruye entre escenas
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayMusic(int musicIndex)
+    {
+        musicSource.clip = musicList[musicIndex];
+        musicSource.Play();
+    }
+
+    public void PlaySFX(int sfxIndex)
+    {
+        sfxSource.PlayOneShot(sfxList[sfxIndex]);
+    }
+}
